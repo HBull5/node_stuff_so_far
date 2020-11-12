714b4ad2af5125bac34ca4399fbdb20a4db03ccf
HBull5 <harcourtbullv@gmail.com>
2020-11-12 15:06:56 -0500

    files basic

--- simple_params/index.js
+++ simple_params/index.js
@@ -1,17 +1,28 @@
 const express = require('express');
 const app = express();
 const PORT = 3000;
-const { StringDecoder } = require('string_decoder');
 const path = require('path');
 const fs = require('fs');
-const model = fs.readFileSync(path.join(__dirname, 'model', 'model.json'), {
-	encoding: 'utf-8'
-});
-
-console.log(model);
+const model = JSON.parse(fs.readFileSync(path.join(__dirname, 'model', 'model.json'), { encoding: 'utf-8' }));
 
 app.set('view engine', 'ejs');
 app.use(express.urlencoded({ extended: true }));
+
+app.get('/', (req, res) => {
+	res.render('login');
+});
+
+app.get('/register', (req, res) => {
+	res.render('register');
+});
+
+app.get('/css/*', (req, res) => {
+	res.sendFile(path.join(__dirname, req.url), err => {
+		if (err) {
+			console.log(err);
+		}
+	});
+});
 
 app.listen(PORT, () => {
 	console.log(`Server running on port ${PORT}...`);
